25f6ba32aeb0b1c5857b4d466102edf72cf316c7
Cassiano <cassianocosta12357@gmail.com>
2025-07-18 21:56:37 -0300

    finalizado

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const props = defineProps(['tarefasPendentes', 'tarefasFinalizadas'])
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ props.tarefasPendentes }} tarefas pendentes
+        </p>
+        <p>
+            Você possui {{ props.tarefasFinalizadas }} tarefas finalizadas
+        </p>
+    </header>
+</template>
